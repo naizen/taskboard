f6a15b4a3b784a918348eba6b6ef1e888f0f40e5
Nai Saevang <naisaevang3@gmail.com>
2019-06-29 14:13:12 -0400

    Added _redirect for netlify

--- src/configs.js
+++ src/configs.js
@@ -1,4 +1,6 @@
-const baseApiUrl =
-  process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL : ''
+//const baseApiUrl =
+//process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL : ''
+
+const baseApiUrl = process.env.REACT_APP_API_URL
 
 export { baseApiUrl }
