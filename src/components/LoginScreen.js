495a11a681fbb681022588972cf953569e61d82f
Nai Saevang <naisaevang3@gmail.com>
2019-06-29 19:06:44 -0400

    Update api url

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -65,8 +65,9 @@ function LoginScreen({ history }) {
           <div className="flex items-center justify-between">
             <button
               className="w-full text-lg bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
-              type="submit">
-              Log In
+              type="submit"
+              disabled={auth.isLoading ? true : false}>
+              {auth.isLoading ? 'Logging In...' : 'Log In'}
             </button>
           </div>
         </form>
--- src/configs.js
+++ src/configs.js
@@ -1,6 +1,4 @@
-//const baseApiUrl =
-//process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL : ''
-
-const baseApiUrl = process.env.REACT_APP_API_URL
+const baseApiUrl =
+  process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL : ''
 
 export { baseApiUrl }
